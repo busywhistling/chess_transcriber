226073134e2b9b9c4743178a93c0d9b164fc1b1d
Paramjit Singh <contact@paramjit.org>
2022-09-29 13:41:52 +0200

    configured jest to be faster

--- jest.config.ts
+++ jest.config.ts
@@ -65,7 +65,7 @@ export default {
 	// globalTeardown: undefined,
 
 	// A set of global variables that need to be available in all test environments
-	// globals: {},
+	globals: {},
 
 	// The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
 	// maxWorkers: "50%",
@@ -167,7 +167,13 @@ export default {
 
 	// A map from regular expressions to paths to transformers
 	transform: {
-		"^.+\\.(ts|tsx|js|jsx)$": "ts-jest",
+		"^.+\\.(ts|tsx|js|jsx)$": [
+			"ts-jest",
+			{
+				// to make jest faster: https://stackoverflow.com/a/60905543
+				isolatedModules: true,
+			},
+		],
 	},
 
 	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
