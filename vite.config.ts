91cc52aa99218ed104112d7cb5616445efb06a65
Paramjit Singh <contact@paramjit.org>
2022-09-29 18:45:46 +0200

    prepare for migration to vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
+// Vite config docs: https://vitejs.dev/config/
+// Vitest config docs: https://vitest.dev/config/
 // FIXME: Disabled minify due to issues processing scss
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
 	base: "https://paramjit.org/chess_transcriber/",
 	build: {
 		minify: false,
+	},
+	test: {
+		include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+		exclude: ["tests/**", "tests-examples/**"], // exclude playwright tests
 	},
 });
